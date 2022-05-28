2fa93476fd760c72f8e3bc8143b7193f70cfca5e
Alex Jones <alexsimonjones@gmail.com>
2022-05-28 10:41:58 +0100

    enables the cancel context

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -13,18 +13,30 @@ func Start(syncr sync.ISync, server service.IService, ctx context.Context) {
 	// This is a very simple example of how the interface can be used for service and sync
 	// The service interface will serve requests whilst the sync interface is responsible
 	// for refreshing the configuration data
+
+	messageBuffer := make(chan string)
+	requestBuffer := make(chan struct{})
+
 	go server.Serve(
-
 		func(ir service.IServiceRequest) service.IServiceResponse {
-
 			if ir.GetRequestType() == service.SERVICE_REQUEST_ALL_FLAGS {
-				data, err := syncr.Fetch()
-				if err != nil {
-					log.Warn(err.Error())
-				}
-				return ir.GenerateServiceResponse(data)
+				requestBuffer <- struct{}{}
+				return ir.GenerateServiceResponse(<-messageBuffer)
 			}
 			return nil
 		})
 
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info("Runtime context has been cancelled")
+			return
+		case <-requestBuffer:
+			data, err := syncr.Fetch()
+			if err != nil {
+				log.Warn(err.Error())
+			}
+			messageBuffer <- data
+		}
+	}
 }
