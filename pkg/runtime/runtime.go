c75102f1fdcf33d319a56386bde44689917c4251
Alex Jones <alexsimonjones@gmail.com>
2022-05-28 10:20:16 +0100

    Fix/hang runtime (#10)
    * security/dependabot/1
    
    * fixing the hang within runtime
    
    * fixing the hang within runtime

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -2,7 +2,6 @@ package runtime
 
 import (
 	"context"
-	"time"
 
 	"github.com/open-feature/flagd/pkg/service"
 	"github.com/open-feature/flagd/pkg/sync"
@@ -14,25 +13,18 @@ func Start(syncr sync.ISync, server service.IService, ctx context.Context) {
 	// This is a very simple example of how the interface can be used for service and sync
 	// The service interface will serve requests whilst the sync interface is responsible
 	// for refreshing the configuration data
-
-	messageBuffer := make(chan string)
-
 	go server.Serve(
 
 		func(ir service.IServiceRequest) service.IServiceResponse {
 
 			if ir.GetRequestType() == service.SERVICE_REQUEST_ALL_FLAGS {
-				return ir.GenerateServiceResponse(<-messageBuffer)
+				data, err := syncr.Fetch()
+				if err != nil {
+					log.Warn(err.Error())
+				}
+				return ir.GenerateServiceResponse(data)
 			}
 			return nil
 		})
 
-	for {
-		data, err := syncr.Fetch()
-		if err != nil {
-			log.Warn(err.Error())
-		}
-		messageBuffer <- data
-		time.Sleep(time.Second * 10)
-	}
 }
