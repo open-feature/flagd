b36eadd41b06bfdba011ea01adf874f8b365ad52
James-Milligan <james@omnant.co.uk>
2022-08-19 12:00:28 +0100

    version command fix
    Signed-off-by: James-Milligan <james@omnant.co.uk>

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +13,18 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of FlagD",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		details, ok := debug.ReadBuildInfo()
+		if ok && details.Main.Version == "(devel)" {
+			Version = details.Main.Version
+			for _, i := range details.Settings {
+				if i.Key == "vcs.time" {
+					Date = i.Value
+				}
+				if i.Key == "vcs.revision" {
+					Commit = i.Value
+				}
+			}
+		}
 		fmt.Printf("flagd %s (%s) built at %s\n", Version, Commit, Date)
 	},
 }
