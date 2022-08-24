e06ea06ce148596ab28b2ee8971e89172dd7c9ef
github-action-benchmark <github@users.noreply.github.com>
2022-08-24 02:50:26 +0000

    add Go Benchmark (go) benchmark result for c72323eb5a3f371a64301fe4c10a368705d6e8e9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661223172298,
+  "lastUpdate": 1661309426164,
   "repoUrl": "https://github.com/open-feature/flagd",
   "entries": {
     "Go Benchmark": [
@@ -82,6 +82,88 @@ window.BENCHMARK_DATA = {
             "value": 6275,
             "unit": "ns/op\t    1288 B/op\t      39 allocs/op",
             "extra": "184858 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "James Milligan",
+            "username": "james-milligan",
+            "email": "75740990+james-milligan@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c72323eb5a3f371a64301fe4c10a368705d6e8e9",
+          "message": "ci: Benchmark threshold update (#154)\n\nSigned-off-by: James-Milligan <james@omnant.co.uk>",
+          "timestamp": "2022-08-24T00:58:01Z",
+          "url": "https://github.com/open-feature/flagd/commit/c72323eb5a3f371a64301fe4c10a368705d6e8e9"
+        },
+        "date": 1661309425536,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGRPCService_ResolveBoolean/happy_path",
+            "value": 2144,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "530707 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveBoolean/eval_returns_error",
+            "value": 6271,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "183297 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveString/happy_path",
+            "value": 2160,
+            "unit": "ns/op\t     256 B/op\t       5 allocs/op",
+            "extra": "523810 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveString/eval_returns_error",
+            "value": 6270,
+            "unit": "ns/op\t    1208 B/op\t      37 allocs/op",
+            "extra": "180550 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveFloat/happy_path",
+            "value": 2161,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "545985 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveFloat/eval_returns_error",
+            "value": 6268,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "184628 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveInt/happy_path",
+            "value": 2151,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "541696 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveInt/eval_returns_error",
+            "value": 6354,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "187074 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveObject/happy_path",
+            "value": 3464,
+            "unit": "ns/op\t    1400 B/op\t      20 allocs/op",
+            "extra": "341629 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveObject/eval_returns_error",
+            "value": 6223,
+            "unit": "ns/op\t    1288 B/op\t      39 allocs/op",
+            "extra": "172928 times\n2 procs"
           }
         ]
       }
