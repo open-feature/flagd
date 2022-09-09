70c93546f032cc7bea8147856789b5562c1eb418
github-action-benchmark <github@users.noreply.github.com>
2022-09-09 02:56:42 +0000

    add Go Benchmark (go) benchmark result for c4e030ed651ab800aa56a555471da5913eb95259

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1662605799537,
+  "lastUpdate": 1662692202810,
   "repoUrl": "https://github.com/open-feature/flagd",
   "entries": {
     "Go Benchmark": [
@@ -1394,6 +1394,88 @@ window.BENCHMARK_DATA = {
             "value": 6026,
             "unit": "ns/op\t    1288 B/op\t      39 allocs/op",
             "extra": "973653 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Alex Jones",
+            "username": "AlexsJones",
+            "email": "alexsimonjones@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c4e030ed651ab800aa56a555471da5913eb95259",
+          "message": "feat: kubernetes sync (#157)\n\nhttps://github.com/open-feature/research/pull/31\r\n<img width=\"1512\" alt=\"Screenshot 2022-08-30 at 15 54 16\" src=\"https://user-images.githubusercontent.com/1235925/187469966-d3c137bd-1f1b-4ee7-9512-848951ec780b.png\">\r\n\r\nSigned-off-by: James-Milligan <james@omnant.co.uk>\r\nSigned-off-by: Alex Jones <alexsimonjones@gmail.com>\r\nSigned-off-by: Alex Jones <alex.jones@canonical.com>\r\nCo-authored-by: James Milligan <75740990+james-milligan@users.noreply.github.com>\r\nCo-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>",
+          "timestamp": "2022-09-06T12:45:19Z",
+          "url": "https://github.com/open-feature/flagd/commit/c4e030ed651ab800aa56a555471da5913eb95259"
+        },
+        "date": 1662692201083,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGRPCService_ResolveBoolean/happy_path",
+            "value": 2655,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "2256415 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveBoolean/eval_returns_error",
+            "value": 7540,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "756637 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveString/happy_path",
+            "value": 2546,
+            "unit": "ns/op\t     256 B/op\t       5 allocs/op",
+            "extra": "2357001 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveString/eval_returns_error",
+            "value": 7653,
+            "unit": "ns/op\t    1208 B/op\t      37 allocs/op",
+            "extra": "789775 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveFloat/happy_path",
+            "value": 2692,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "2217356 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveFloat/eval_returns_error",
+            "value": 7690,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "730690 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveInt/happy_path",
+            "value": 2649,
+            "unit": "ns/op\t     240 B/op\t       5 allocs/op",
+            "extra": "2325058 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveInt/eval_returns_error",
+            "value": 7562,
+            "unit": "ns/op\t    1192 B/op\t      37 allocs/op",
+            "extra": "777019 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveObject/happy_path",
+            "value": 4065,
+            "unit": "ns/op\t    1400 B/op\t      20 allocs/op",
+            "extra": "1467740 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkGRPCService_ResolveObject/eval_returns_error",
+            "value": 7084,
+            "unit": "ns/op\t    1288 B/op\t      39 allocs/op",
+            "extra": "807318 times\n2 procs"
           }
         ]
       }
