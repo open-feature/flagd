837b0c673e7e7d4799f100291ca520d22944f22a
Alex Jones <alexsimonjones@gmail.com>
2022-10-13 12:40:51 +0100

    feat: fixing informer issues (#191)
    This completely reimplements the informer system. 
    It now avoids the issues using the cache informer watcher which
    occasionally ommits errors - to my best reckoning it is due to partial
    object updates not deserialising properly.
    
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

--- pkg/sync/kubernetes/kubernetes_sync.go
+++ pkg/sync/kubernetes/kubernetes_sync.go
@@ -2,17 +2,23 @@ package kubernetes
 
 import (
 	"context"
+	"errors"
 	"os"
 	"time"
 
 	"github.com/open-feature/flagd/pkg/sync"
-	"github.com/open-feature/flagd/pkg/sync/kubernetes/featureflagconfiguration"
-	ffv1alpha1 "github.com/open-feature/open-feature-operator/apis/core/v1alpha1"
+	"github.com/open-feature/open-feature-operator/apis/core/v1alpha1"
 	log "github.com/sirupsen/logrus"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
-	controllerClient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -20,12 +26,12 @@ const (
 	featureFlagNamespaceName     = "namespace"
 )
 
-var resyncPeriod time.Duration // default of 0
+var resyncPeriod = 1 * time.Minute
 
 type Sync struct {
 	Logger       *log.Entry
 	ProviderArgs sync.ProviderArgs
-	client       *featureflagconfiguration.FFCClient
+	client       client.Client
 }
 
 func (k *Sync) Source() string {
@@ -48,18 +54,29 @@ func (k *Sync) Fetch(ctx context.Context) (string, error) {
 		return "{}", nil
 	}
 
-	config, err := k.client.FeatureFlagConfigurations(k.ProviderArgs[featureFlagNamespaceName]).
-		Get(k.ProviderArgs[featureFlagConfigurationName], metav1.GetOptions{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "FeatureFlagConfiguration",
-				APIVersion: "featureflag.open-feature.io/v1alpha1",
-			},
-		})
+	var ff v1alpha1.FeatureFlagConfiguration
+	err := k.client.Get(ctx, client.ObjectKey{
+		Name:      k.ProviderArgs[featureFlagConfigurationName],
+		Namespace: k.ProviderArgs[featureFlagNamespaceName],
+	}, &ff)
+
+	return ff.Spec.FeatureFlagSpec, err
+}
+
+func (k *Sync) buildConfiguration() (*rest.Config, error) {
+	kubeconfig := os.Getenv("KUBECONFIG")
+	var clusterConfig *rest.Config
+	var err error
+	if kubeconfig != "" {
+		clusterConfig, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+	} else {
+		clusterConfig, err = rest.InClusterConfig()
+	}
 	if err != nil {
-		return "{}", err
+		return nil, err
 	}
 
-	return config.Spec.FeatureFlagSpec, nil
+	return clusterConfig, nil
 }
 
 func (k *Sync) Notify(ctx context.Context, c chan<- sync.INotify) {
@@ -72,36 +89,121 @@ func (k *Sync) Notify(ctx context.Context, c chan<- sync.INotify) {
 		return
 	}
 	k.Logger.Infof("Starting kubernetes sync notifier for resource %s", k.ProviderArgs["featureflagconfiguration"])
-	kubeconfig := os.Getenv("KUBECONFIG")
 
-	// Create the client configuration
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	clusterConfig, err := k.buildConfiguration()
 	if err != nil {
+		k.Logger.Errorf("Error building configuration: %s", err)
+	}
+	if err := v1alpha1.AddToScheme(scheme.Scheme); err != nil {
 		k.Logger.Panic(err.Error())
 	}
-
-	if k.ProviderArgs["resyncperiod"] != "" {
-		hr, err := time.ParseDuration(k.ProviderArgs["resyncperiod"])
-		if err != nil {
-			k.Logger.Panic(err.Error())
-		}
-		resyncPeriod = hr
-	}
-
-	k.client, err = featureflagconfiguration.NewForConfig(config)
+	k.client, err = client.New(clusterConfig, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
-		k.Logger.Panic(err.Error())
+		k.Logger.Fatalln(err)
 	}
-
-	if err := ffv1alpha1.AddToScheme(scheme.Scheme); err != nil {
-		k.Logger.Panic(err.Error())
+	clusterClient, err := dynamic.NewForConfig(clusterConfig)
+	if err != nil {
+		log.Fatalln(err)
 	}
+	resource := v1alpha1.GroupVersion.WithResource("featureflagconfigurations")
+	// The created informer will not do resyncs if the given
+	// defaultEventHandlerResyncPeriod is zero.
+	// For more details on resync implications refer to tools/cache/shared_informer.go
+	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(clusterClient,
+		resyncPeriod, corev1.NamespaceAll, nil)
+	informer := factory.ForResource(resource).Informer()
 
-	go featureflagconfiguration.WatchResources(ctx, *k.Logger.WithFields(log.Fields{
-		"sync":      "kubernetes",
-		"component": "watchresources",
-	}), k.client, resyncPeriod, controllerClient.ObjectKey{
+	objectKey := client.ObjectKey{
 		Name:      k.ProviderArgs[featureFlagConfigurationName],
 		Namespace: k.ProviderArgs[featureFlagNamespaceName],
-	}, c)
+	}
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			if err := createFuncHandler(obj, objectKey, c); err != nil {
+				k.Logger.Warn(err.Error())
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			if err := updateFuncHandler(oldObj, newObj, objectKey, c); err != nil {
+				k.Logger.Warn(err.Error())
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			if err := deleteFuncHandler(obj, objectKey, c); err != nil {
+				k.Logger.Warn(err.Error())
+			}
+		},
+	})
+
+	informer.Run(ctx.Done())
+}
+
+func createFuncHandler(obj interface{}, object client.ObjectKey, c chan<- sync.INotify) error {
+	var ffObj v1alpha1.FeatureFlagConfiguration
+	u := obj.(*unstructured.Unstructured)
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &ffObj)
+	if err != nil {
+		return err
+	}
+	if ffObj.APIVersion != v1alpha1.GroupVersion.Version {
+		return errors.New("invalid api version")
+	}
+	if ffObj.Name == object.Name {
+		c <- &sync.Notifier{
+			Event: sync.Event[sync.DefaultEventType]{
+				EventType: sync.DefaultEventTypeCreate,
+			},
+		}
+	}
+	return nil
+}
+
+func updateFuncHandler(oldObj interface{}, newObj interface{}, object client.ObjectKey, c chan<- sync.INotify) error {
+	var ffOldObj v1alpha1.FeatureFlagConfiguration
+	u := oldObj.(*unstructured.Unstructured)
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &ffOldObj)
+	if err != nil {
+		return err
+	}
+	if ffOldObj.APIVersion != v1alpha1.GroupVersion.Version {
+		return errors.New("invalid api version")
+	}
+	var ffNewObj v1alpha1.FeatureFlagConfiguration
+	u = newObj.(*unstructured.Unstructured)
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &ffNewObj)
+	if err != nil {
+		return err
+	}
+	if ffNewObj.APIVersion != v1alpha1.GroupVersion.Version {
+		return errors.New("invalid api version")
+	}
+	if object.Name == ffNewObj.Name && ffOldObj.ResourceVersion != ffNewObj.ResourceVersion {
+		// Only update if there is an actual featureFlagSpec change
+		c <- &sync.Notifier{
+			Event: sync.Event[sync.DefaultEventType]{
+				EventType: sync.DefaultEventTypeModify,
+			},
+		}
+	}
+	return nil
+}
+
+func deleteFuncHandler(obj interface{}, object client.ObjectKey, c chan<- sync.INotify) error {
+	var ffObj v1alpha1.FeatureFlagConfiguration
+	u := obj.(*unstructured.Unstructured)
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &ffObj)
+	if err != nil {
+		return err
+	}
+	if ffObj.APIVersion != v1alpha1.GroupVersion.Version {
+		return errors.New("invalid api version")
+	}
+	if ffObj.Name == object.Name {
+		c <- &sync.Notifier{
+			Event: sync.Event[sync.DefaultEventType]{
+				EventType: sync.DefaultEventTypeDelete,
+			},
+		}
+	}
+	return nil
 }
