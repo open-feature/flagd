48256342e767efe0b8e3c99bfe0d336f51e0e3a6
James Milligan <james@omnant.co.uk>
2022-11-17 10:37:46 +0000

    added comment describing functionality of package
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,6 +6,33 @@ import (
 	"go.uber.org/zap"
 )
 
+/*
+This package wraps the zap logging API for use across services without passing a shared logger.
+Fields can be added to a requestID using the WriteFields method, these will be added to any
+subsequent XxxWithID log calls. To preserve performance ClearFields must be called when the
+requestID's thread is closed as a sync map is used internally.
+Child loggers can be created from a parent logger using the WithFields method, this child logger
+will append the provided fields to all logs, whilst maintaining a reference to the top level
+request fields pool.
+
+Example:
+
+WrappedLogger := NewLogger(myLogger)
+WrappedLogger.WriteFields("my-id", zap.String("foo", "bar"))
+WrappedLogger2 := WrappedLogger.WithFields(zap.String("ping", "pong"))
+
+WrappedLogger.DebugWithID("my-id", "my log line")
+	=> {"level":"debug","foo":"bar","msg":"my log line""}
+
+WrappedLogger2.DebugWithID("my-id", "my log line")
+	=> {"level":"debug","foo":"bar","ping":"pong","msg":"my log line""}
+
+WrappedLogger2.WriteFields("my-id", zap.String("food", "bars"))
+
+WrappedLogger.DebugWithID("my-id", "my log line")
+	=> {"level":"debug","foo":"bar","food":"bars","msg":"my log line""}
+*/
+
 type Logger struct {
 	requestFields *sync.Map
 	Logger        *zap.Logger
