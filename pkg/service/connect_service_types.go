eef5e8c8d3c31fd1ebabf155ed4e441dbabb79d5
Skye Gill <gill.skye95@gmail.com>
2023-02-08 13:09:03 +0000

    refactor: simplify connect Resolve... funcs
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

---
+++ pkg/service/connect_service_types.go
@@ -0,0 +1,69 @@
+package service
+
+import (
+	"fmt"
+
+	schemaV1 "buf.build/gen/go/open-feature/flagd/protocolbuffers/go/schema/v1"
+	"github.com/bufbuild/connect-go"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+type booleanResponse struct {
+	*connect.Response[schemaV1.ResolveBooleanResponse]
+}
+
+func (r *booleanResponse) SetResult(value bool, variant, reason string) error {
+	r.Msg.Value = value
+	r.Msg.Variant = variant
+	r.Msg.Reason = reason
+	return nil
+}
+
+type stringResponse struct {
+	*connect.Response[schemaV1.ResolveStringResponse]
+}
+
+func (r *stringResponse) SetResult(value, variant, reason string) error {
+	r.Msg.Value = value
+	r.Msg.Variant = variant
+	r.Msg.Reason = reason
+	return nil
+}
+
+type floatResponse struct {
+	*connect.Response[schemaV1.ResolveFloatResponse]
+}
+
+func (r *floatResponse) SetResult(value float64, variant, reason string) error {
+	r.Msg.Value = value
+	r.Msg.Variant = variant
+	r.Msg.Reason = reason
+	return nil
+}
+
+type intResponse struct {
+	*connect.Response[schemaV1.ResolveIntResponse]
+}
+
+func (r *intResponse) SetResult(value int64, variant, reason string) error {
+	r.Msg.Value = value
+	r.Msg.Variant = variant
+	r.Msg.Reason = reason
+	return nil
+}
+
+type objectResponse struct {
+	*connect.Response[schemaV1.ResolveObjectResponse]
+}
+
+func (r *objectResponse) SetResult(value map[string]any, variant, reason string) error {
+	r.Msg.Reason = reason
+	val, err := structpb.NewStruct(value)
+	if err != nil {
+		return fmt.Errorf("struct response construction: %w", err)
+	}
+
+	r.Msg.Value = val
+	r.Msg.Variant = variant
+	return nil
+}
