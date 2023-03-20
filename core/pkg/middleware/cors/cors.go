c7b41f1635e8fad25b1382b5aec0744b15430d18
Florian Bacher <florian.bacher@dynatrace.com>
2023-03-20 11:43:19 +0100

    chore: refactor middleware setup
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

---
+++ core/pkg/middleware/cors/cors.go
@@ -0,0 +1,45 @@
+package cors
+
+import (
+	"github.com/rs/cors"
+	"net/http"
+)
+
+type Middleware struct {
+	cors *cors.Cors
+}
+
+func New(allowedOrigins []string) *Middleware {
+	return &Middleware{
+		cors: cors.New(cors.Options{
+			AllowedMethods: []string{
+				http.MethodHead,
+				http.MethodGet,
+				http.MethodPost,
+				http.MethodPut,
+				http.MethodPatch,
+				http.MethodDelete,
+			},
+			AllowedOrigins: allowedOrigins,
+			AllowedHeaders: []string{"*"},
+			ExposedHeaders: []string{
+				// Content-Type is in the default safelist.
+				"Accept",
+				"Accept-Encoding",
+				"Accept-Post",
+				"Connect-Accept-Encoding",
+				"Connect-Content-Encoding",
+				"Content-Encoding",
+				"Grpc-Accept-Encoding",
+				"Grpc-Encoding",
+				"Grpc-Message",
+				"Grpc-Status",
+				"Grpc-Status-Details-Bin",
+			},
+		}),
+	}
+}
+
+func (c Middleware) Handle(handler http.Handler) http.Handler {
+	return c.cors.Handler(handler)
+}
---
+++ core/pkg/middleware/h2c/h2c.go
@@ -0,0 +1,17 @@
+package h2c
+
+import (
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"net/http"
+)
+
+type Middleware struct{}
+
+func New() *Middleware {
+	return &Middleware{}
+}
+
+func (m Middleware) Handle(handler http.Handler) http.Handler {
+	return h2c.NewHandler(handler, &http2.Server{})
+}
