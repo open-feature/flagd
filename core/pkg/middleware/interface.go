4ac22ae2becbf61eebdc13e357698463c9094e57
Florian Bacher <florian.bacher@dynatrace.com>
2023-03-20 15:41:28 +0100

    remove obsolete code
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- core/pkg/middleware/interface.go
+++ core/pkg/middleware/interface.go
@@ -1,19 +1,9 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 )
 
 type Middleware interface {
 	Handle(handler http.Handler) http.Handler
-}
-
-type Logger struct{}
-
-func (Logger) Handle(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("log")
-		handler.ServeHTTP(writer, request)
-	})
 }
