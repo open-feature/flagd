425de9a1c2d1574779b905ac6debb9edfc156b15
James Milligan <75740990+james-milligan@users.noreply.github.com>
2023-04-11 13:08:57 +0100

    refactor: remove connect-go from flagd-proxy and replace with grpc (#589)
    <!-- Please use this template for your pull request. -->
    <!-- Please use the sections that you need and delete other sections -->
    
    ## This PR
    <!-- add the description of the PR here -->
    
    - during load testing of the flags-proxy it was found that connect-go
    causes `connection reset by peer` errors when there are too many
    simultaneous connections, this is not the case when only using grpc.
    This PR removes connect-go, replacing it with a standalone grpc server
    - the `fetchAllFlags` endpoint will no longer be available via a http
    request, this was only used internally by the flagd grpc sync
    implementation
    
    ### Related Issues
    <!-- add here the GitHub issue that this PR resolves if applicable -->
    
    
    ### Notes
    <!-- any additional notes for this PR -->
    
    ### Follow-up Tasks
    <!-- anything that is related to this PR but not done here should be
    noted under this section -->
    <!-- if there is a need for a new issue, please link it here -->
    
    ### How to test
    <!-- if applicable, add testing instructions under this section -->
    
    ---------
    
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- core/pkg/service/sync/handler.go
+++ core/pkg/service/sync/handler.go
@@ -3,40 +3,42 @@ package sync
 import (
 	"context"
 
+	rpc "buf.build/gen/go/open-feature/flagd/grpc/go/sync/v1/syncv1grpc"
 	syncv1 "buf.build/gen/go/open-feature/flagd/protocolbuffers/go/sync/v1"
-	"github.com/bufbuild/connect-go"
 	"github.com/open-feature/flagd/core/pkg/logger"
 	"github.com/open-feature/flagd/core/pkg/sync"
 	syncStore "github.com/open-feature/flagd/core/pkg/sync-store"
 )
 
 type handler struct {
+	rpc.UnimplementedFlagSyncServiceServer
 	syncStore *syncStore.SyncStore
 	logger    *logger.Logger
 }
 
-func (l *handler) FetchAllFlags(ctx context.Context, req *connect.Request[syncv1.FetchAllFlagsRequest]) (
-	*connect.Response[syncv1.FetchAllFlagsResponse],
+func (l *handler) FetchAllFlags(ctx context.Context, req *syncv1.FetchAllFlagsRequest) (
+	*syncv1.FetchAllFlagsResponse,
 	error,
 ) {
-	data, err := l.syncStore.FetchAllFlags(ctx, req, req.Msg.GetSelector())
+	data, err := l.syncStore.FetchAllFlags(ctx, req, req.GetSelector())
 	if err != nil {
-		return connect.NewResponse(&syncv1.FetchAllFlagsResponse{}), err
+		return &syncv1.FetchAllFlagsResponse{}, err
 	}
 
-	return connect.NewResponse(&syncv1.FetchAllFlagsResponse{
+	return &syncv1.FetchAllFlagsResponse{
 		FlagConfiguration: data.FlagData,
-	}), nil
+	}, nil
 }
 
 func (l *handler) SyncFlags(
-	ctx context.Context,
-	req *connect.Request[syncv1.SyncFlagsRequest],
-	stream *connect.ServerStream[syncv1.SyncFlagsResponse],
+	req *syncv1.SyncFlagsRequest,
+	stream rpc.FlagSyncService_SyncFlagsServer,
 ) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	errChan := make(chan error)
 	dataSync := make(chan sync.DataSync)
-	l.syncStore.RegisterSubscription(ctx, req.Msg.GetSelector(), req, dataSync, errChan)
+	l.syncStore.RegisterSubscription(ctx, req.GetSelector(), req, dataSync, errChan)
 	for {
 		select {
 		case e := <-errChan:
