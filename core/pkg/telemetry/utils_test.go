e3c8ac7df008352dd046155de4b4cd9e56690abc
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-04-12 09:30:33 -0700

    lint fixes
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- core/pkg/telemetry/utils_test.go
+++ core/pkg/telemetry/utils_test.go
@@ -1,12 +1,13 @@
 package telemetry
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/require"
 	semconv "go.opentelemetry.io/otel/semconv/v1.18.0"
-	"testing"
 )
 
-func TestSemConvFeatureFlagAttributes(T *testing.T) {
+func TestSemConvFeatureFlagAttributes(t *testing.T) {
 	tests := []struct {
 		name    string
 		key     string
@@ -32,19 +33,18 @@ func TestSemConvFeatureFlagAttributes(T *testing.T) {
 		for _, attribute := range attributes {
 			switch attribute.Key {
 			case semconv.FeatureFlagKeyKey:
-				require.Equal(T, test.key, attribute.Value.AsString(),
+				require.Equal(t, test.key, attribute.Value.AsString(),
 					"expected flag key: %s, but received: %s", test.key, attribute.Value.AsString())
 			case semconv.FeatureFlagVariantKey:
-				require.Equal(T, test.variant, attribute.Value.AsString(),
+				require.Equal(t, test.variant, attribute.Value.AsString(),
 					"expected flag variant: %s, but received %s", test.variant, attribute.Value.AsString())
 			case semconv.FeatureFlagProviderNameKey:
-				require.Equal(T, provider, attribute.Value.AsString(),
+				require.Equal(t, provider, attribute.Value.AsString(),
 					"expected flag provider: %s, but received %s", provider, attribute.Value.AsString())
 			default:
-				T.Errorf("attributes contains unexpected attribute. with key: %v, with type: %v",
+				t.Errorf("attributes contains unexpected attribute. with key: %v, with type: %v",
 					attribute.Key, attribute.Value.Type())
 			}
 		}
 	}
-
 }
