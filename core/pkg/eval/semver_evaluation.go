251712630c7e1414510a8535e58dd65a4e4507cf
Florian Bacher <florian.bacher@dynatrace.com>
2023-05-24 14:32:47 +0200

    implementation of sem_ver evaluation
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- core/pkg/eval/semver_evaluation.go
+++ core/pkg/eval/semver_evaluation.go
@@ -1,9 +1,47 @@
 package eval
 
 import (
+	"errors"
 	"fmt"
+	"github.com/open-feature/flagd/core/pkg/logger"
+	"golang.org/x/mod/semver"
 	"strings"
 )
+
+type SemVerOperator string
+
+const (
+	Equals         SemVerOperator = "="
+	NotEqual       SemVerOperator = "!="
+	Less           SemVerOperator = "<"
+	LessOrEqual    SemVerOperator = "<="
+	GreaterOrEqual SemVerOperator = ">="
+	Greater        SemVerOperator = ">"
+)
+
+func (svo SemVerOperator) Compare(v1, v2 string) (bool, error) {
+	cmpRes := semver.Compare(v1, v2)
+	switch svo {
+	case Less:
+		return cmpRes == -1, nil
+	case Equals:
+		return cmpRes == 0, nil
+	case NotEqual:
+		return cmpRes != 0, nil
+	case LessOrEqual:
+		return cmpRes == -1 || cmpRes == 0, nil
+	case GreaterOrEqual:
+		return cmpRes == +1 || cmpRes == 0, nil
+	case Greater:
+		return cmpRes == +1, nil
+	default:
+		return false, errors.New("invalid operator")
+	}
+}
+
+type SemVerComparisonEvaluator struct {
+	Logger *logger.Logger
+}
 
 // semVerEvaluation checks if the given property matches a semantic versioning condition.
 // It returns 'true', if the value of the given property meets the condition, 'false' if not.
@@ -12,7 +50,7 @@ import (
 //	{
 //	  "if": [
 //			{
-//				"starts_with": [{"var": "version"}, ">=" "1.0.0"]
+//				"sem_ver": [{"var": "version"}, ">=" "1.0.0"]
 //			},
 //			"red", null
 //			]
@@ -20,15 +58,76 @@ import (
 //
 // This rule can be applied to the following data object, where the evaluation will resolve to 'true':
 //
-// { "email": "2.0.0" }
+// { "version": "2.0.0" }
 //
-// Note that the 'starts_with'  evaluation rule must contain exactly two items, which both resolve to a
-// string value
-func (je *JSONEvaluator) startsWithEvaluation(values, _ interface{}) interface{} {
-	propertyValue, target, err := parseStringComparisonEvaluationData(values)
+// Note that the 'sem_ver' evaluation rule must contain exactly three items:
+// 1. Target property: this needs which both resolve to a semantic versioning string
+// 2. Operator: One of the following: '=', '!=', '>', '<', '>=', '<='
+// 3. Target value: this needs which both resolve to a semantic versioning string
+func (je *JSONEvaluator) semVerEvaluation(values, _ interface{}) interface{} {
+	actualVersion, targetVersion, operator, err := parseSemverEvaluationData(values)
 	if err != nil {
-		je.Logger.Error(fmt.Sprintf("parse starts_with evaluation data: %v", err))
+		je.Logger.Error(fmt.Sprintf("parse sem_ver evaluation data: %v", err))
 		return nil
 	}
-	return strings.HasPrefix(propertyValue, target)
+	res, err := operator.Compare(actualVersion, targetVersion)
+	if err != nil {
+		je.Logger.Error(fmt.Sprintf("sem_ver evaluation: %v", err))
+		return nil
+	}
+	return res
+}
+
+func parseSemverEvaluationData(values interface{}) (string, string, SemVerOperator, error) {
+	parsed, ok := values.([]interface{})
+	if !ok {
+		return "", "", "", errors.New("sem_ver evaluation is not an array")
+	}
+
+	if len(parsed) != 2 {
+		return "", "", "", errors.New("sem_ver evaluation must contain a value, an operator and a comparison target")
+	}
+
+	actualVersion, err := parseSemanticVersion(parsed[0])
+	if err != nil {
+		return "", "", "", fmt.Errorf("sem_ver evaluation: could not parse target property value: %w", err)
+	}
+
+	operator, err := parseOperator(parsed[1])
+	if err != nil {
+		return "", "", "", fmt.Errorf("sem_ver evaluation: could not parse operator: %w", err)
+	}
+
+	targetVersion, err := parseSemanticVersion(parsed[2])
+	if err != nil {
+		return "", "", "", fmt.Errorf("sem_ver evaluation: could not parse target value: %w", err)
+	}
+	return actualVersion, targetVersion, operator, nil
+}
+
+func parseSemanticVersion(v interface{}) (string, error) {
+	version, ok := v.(string)
+	if !ok {
+		return "", errors.New("sem_ver evaluation: property did not resolve to a string value")
+	}
+	// version strings are only valid in the semver package if they start with a 'v'
+	// if it's not present in the given value, we prepend it
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
+	}
+
+	if !semver.IsValid(version) {
+		return "", errors.New("not a valid semantic version string")
+	}
+
+	return version, nil
+}
+
+func parseOperator(o interface{}) (SemVerOperator, error) {
+	operatorString, ok := o.(string)
+	if !ok {
+		return "", errors.New("could not parse operator")
+	}
+
+	return SemVerOperator(operatorString), nil
 }
