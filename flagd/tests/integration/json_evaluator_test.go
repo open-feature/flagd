6f83942786434630cd45013e912712e26714a148
Todd Baert <todd.baert@dynatrace.com>
2023-10-13 09:56:31 -0400

    fixup: absorb test suite changes
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- flagd/tests/integration/json_evaluator_test.go
+++ flagd/tests/integration/json_evaluator_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cucumber/godog"
 	flagd "github.com/open-feature/go-sdk-contrib/providers/flagd/pkg"
 	"github.com/open-feature/go-sdk-contrib/tests/flagd/pkg/integration"
+	"github.com/open-feature/go-sdk/pkg/openfeature"
 )
 
 func TestJsonEvaluator(t *testing.T) {
@@ -20,8 +21,10 @@ func TestJsonEvaluator(t *testing.T) {
 	name := "flagd-json-evaluator.feature"
 
 	testSuite := godog.TestSuite{
-		Name:                name,
-		ScenarioInitializer: integration.InitializeFlagdJsonScenario(providerOptions...),
+		Name: name,
+		ScenarioInitializer: integration.InitializeFlagdJsonScenario(func() openfeature.FeatureProvider {
+			return flagd.NewProvider(providerOptions...)
+		}),
 		Options: &godog.Options{
 			Format:   "pretty",
 			Paths:    []string{"../../../test-harness/gherkin/flagd-json-evaluator.feature"},
