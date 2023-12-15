a607dad96165b58548b2f4f0c9f4de8b15717db5
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-12-15 15:05:23 -0500

    update copy for the time based scenario
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- playground-app/src/scenarios/enable-by-time.ts
+++ playground-app/src/scenarios/enable-by-time.ts
@@ -4,8 +4,9 @@ import { contextToPrettyJson, featureDefinitionToPrettyJson } from "../utils";
 export const enableByTime: Scenario = {
   description: [
     'In this scenario, we have a feature flag with the key "enable-announcement-banner" that is enabled and has two variants: true and false.',
-    "This flag has a targeting rule defined that enables the flag for users when the current time is greater or equal to the scheduled release.",
-    "This feature schedule for release 5 seconds after the scenario is loaded.",
+    "This flag has a targeting rule defined that enables the flag after a specified time.",
+    'The current time (epoch) can be accessed using "$flagd.timestamp" which is automatically provided by flagd.',
+    'Five seconds after loading this scenario, the response will change to "true".',
   ].join(" "),
   flagDefinition: () =>
     featureDefinitionToPrettyJson({
