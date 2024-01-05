5c5191ae23791a922fc869bdd10984a0257aa191
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-01-05 14:33:20 -0500

    update the fractional playground description
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- playground-app/src/scenarios/fraction-string.ts
+++ playground-app/src/scenarios/fraction-string.ts
@@ -3,9 +3,9 @@ import { contextToPrettyJson, featureDefinitionToPrettyJson } from "../utils";
 
 export const pseudoRandomSplit: Scenario = {
   description: [
-    'In this scenario, we have a feature flag with the key "pseudo-random-split" that is enabled and has four variants: red, blue, green, and grey.',
+    'In this scenario, we have a feature flag with the key "color-palette-experiment" that is enabled and has four variants: red, blue, green, and grey.',
     'The targeting rule uses the "fractional" operator, which deterministically splits the traffic based on the configuration.',
-    "This configuration splits the traffic evenly between the four variants but notice that the targeting rule is based on the sessionId, which means that the same user will always get the same variant.",
+    'This configuration splits the traffic evenly between the four variants by bucketing evaluations using the "targetingKey" and feature flag key.',
     'Experiment by changing the "targetingKey" to another value.',
   ].join(" "),
   flagDefinition: featureDefinitionToPrettyJson({
