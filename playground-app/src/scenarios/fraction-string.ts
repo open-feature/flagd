3bd47aa5853a7f46b5a7d96901e92838bfe220e2
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-01-08 09:42:47 -0500

    apply PR feedback
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- playground-app/src/scenarios/fraction-string.ts
+++ playground-app/src/scenarios/fraction-string.ts
@@ -5,7 +5,7 @@ export const pseudoRandomSplit: Scenario = {
   description: [
     'In this scenario, we have a feature flag with the key "color-palette-experiment" that is enabled and has four variants: red, blue, green, and grey.',
     'The targeting rule uses the "fractional" operator, which deterministically splits the traffic based on the configuration.',
-    'This configuration splits the traffic evenly between the four variants by bucketing evaluations using the "targetingKey" and feature flag key.',
+    'This configuration splits the traffic evenly between the four variants by bucketing evaluations pseudorandomly using the "targetingKey" and feature flag key.',
     'Experiment by changing the "targetingKey" to another value.',
   ].join(" "),
   flagDefinition: featureDefinitionToPrettyJson({
