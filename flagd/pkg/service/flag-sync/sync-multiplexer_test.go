6c491fc2957c9941e2af6a98eb8ec4ada9b36751
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2024-03-06 12:31:08 -0800

    unit tests
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

---
+++ flagd/pkg/service/flag-sync/sync-multiplexer_test.go
@@ -0,0 +1,208 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/open-feature/flagd/core/pkg/model"
+	"github.com/open-feature/flagd/core/pkg/store"
+)
+
+func TestRegistration(t *testing.T) {
+	// given
+	mux := newMux(getSimpleFlagStore())
+	err := mux.extract()
+	if err != nil {
+		t.Fatal("error during flag extraction")
+		return
+	}
+
+	tests := []struct {
+		testName    string
+		id          interface{}
+		source      string
+		connection  chan payload
+		expectError bool
+	}{
+		{
+			testName:   "subscribe to all flags",
+			id:         context.Background(),
+			connection: make(chan payload, 1),
+		},
+		{
+			testName:   "subscribe to source A",
+			id:         context.Background(),
+			source:     "A",
+			connection: make(chan payload, 1),
+		},
+		{
+			testName:   "subscribe to source B",
+			id:         context.Background(),
+			source:     "B",
+			connection: make(chan payload, 1),
+		},
+		{
+			testName:    "subscribe to non-existing",
+			id:          context.Background(),
+			source:      "C",
+			connection:  make(chan payload, 1),
+			expectError: true,
+		},
+	}
+
+	// validate registration
+	for _, test := range tests {
+		t.Run(test.testName, func(t *testing.T) {
+			// when
+			err := mux.register(test.id, test.source, test.connection)
+
+			// then
+			if !test.expectError && err != nil {
+				t.Fatal("expected no errors, but got error")
+			}
+
+			if test.expectError && err != nil {
+				// pass
+				return
+			}
+
+			// validate subscription
+			var initSync payload
+			select {
+			case <-time.After(2 * time.Second):
+				t.Fatal("data sync did not complete for initial sync within an acceptable timeframe")
+
+			case initSync = <-test.connection:
+				break
+			}
+
+			if initSync.flags == "" {
+				t.Fatal("expected non empty flag payload, but got empty")
+			}
+
+			// validate source of flag
+			if test.source != "" && !strings.Contains(initSync.flags, fmt.Sprintf("\"source\":\"%s\"", test.source)) {
+				t.Fatal("expected initial flag response to contain flags from source, but failed to find source")
+			}
+		})
+	}
+}
+
+func TestUpdateAndRemoval(t *testing.T) {
+	// given
+	mux := newMux(getSimpleFlagStore())
+	err := mux.extract()
+	if err != nil {
+		t.Fatal("error during flag extraction")
+		return
+	}
+
+	identifier := context.Background()
+	channel := make(chan payload, 1)
+	err = mux.register(identifier, "", channel)
+	if err != nil {
+		t.Fatal("error during subscription registration")
+		return
+	}
+
+	select {
+	case <-time.After(2 * time.Second):
+		t.Fatal("data sync did not complete for initial sync within an acceptable timeframe")
+	case <-channel:
+		break
+	}
+
+	// when - updates are triggered
+	err = mux.pushUpdates()
+	if err != nil {
+		t.Fatal("failure to trigger update request on multiplexer")
+		return
+	}
+
+	// then
+	select {
+	case <-time.After(2 * time.Second):
+		t.Fatal("data sync did not complete for initial sync within an acceptable timeframe")
+	case <-channel:
+		break
+	}
+
+	// when - subscription removed & update triggered
+	mux.unregister(identifier, "")
+	err = mux.pushUpdates()
+	if err != nil {
+		t.Fatal("failure to trigger update request on multiplexer")
+		return
+	}
+
+	// then
+	select {
+	case <-time.After(2 * time.Second):
+		break
+	case <-channel:
+		t.Fatal("expected no sync but got an update as removal was not performed")
+	}
+}
+
+func TestGetAllFlags(t *testing.T) {
+	// given
+	mux := newMux(getSimpleFlagStore())
+	err := mux.extract()
+	if err != nil {
+		t.Fatal("error during flag extraction")
+		return
+	}
+
+	// when - get all with open scope
+	flags, err := mux.getALlFlags("")
+	if err != nil {
+		t.Fatal("error when retrieving all flags")
+		return
+	}
+
+	if len(flags) == 0 {
+		t.Fatal("expected no empty flags")
+		return
+	}
+
+	// when - get all with a scope
+	flags, err = mux.getALlFlags("A")
+	if err != nil {
+		t.Fatal("error when retrieving all flags")
+		return
+	}
+
+	if len(flags) == 0 || !strings.Contains(flags, fmt.Sprintf("\"source\":\"%s\"", "A")) {
+		t.Fatal("expected flags to be scoped")
+		return
+	}
+}
+
+// getSimpleFlagStore returns a flag store pre-filled with flags from sources A & B
+func getSimpleFlagStore() (*store.Flags, []string) {
+	variants := map[string]any{
+		"true":  true,
+		"false": false,
+	}
+
+	flagStore := store.NewFlags()
+
+	flagStore.Set("flagA", model.Flag{
+		State:          "ENABLED",
+		DefaultVariant: "false",
+		Variants:       variants,
+		Source:         "A",
+	})
+
+	flagStore.Set("flagB", model.Flag{
+		State:          "ENABLED",
+		DefaultVariant: "true",
+		Variants:       variants,
+		Source:         "B",
+	})
+
+	return flagStore, []string{"A", "B"}
+}
