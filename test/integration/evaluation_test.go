ab358fadc306be9c4e561a052f6ac66d2b2b6b6e
Pradeep <pradeepbbl@gmail.com>
2024-11-15 10:27:00 +0530

    fix: update e2e tests
    Signed-off-by: Pradeep <pradeepbbl@gmail.com>

--- test/integration/evaluation_test.go
+++ test/integration/evaluation_test.go
@@ -43,3 +43,40 @@ func TestEvaluation(t *testing.T) {
 		t.Fatal("non-zero status returned, failed to run evaluation tests")
 	}
 }
+
+func TestEvaluationUsingEnvoy(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	flag.Parse()
+
+	var providerOptions []flagd.ProviderOption
+	name := "evaluation.feature"
+
+	if tls == "true" {
+		name = "evaluation_tls.feature"
+		providerOptions = []flagd.ProviderOption{
+			flagd.WithTLS(certPath),
+			flagd.WithTargetUri("envoy://localhost:9211/flagd-sync.service"),
+		}
+	}
+
+	testSuite := godog.TestSuite{
+		Name: name,
+		TestSuiteInitializer: integration.InitializeTestSuite(func() openfeature.FeatureProvider {
+			return flagd.NewProvider(providerOptions...)
+		}),
+		ScenarioInitializer: integration.InitializeEvaluationScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"../../spec/specification/assets/gherkin/evaluation.feature"},
+			TestingT: t, // Testing instance that will run subtests.
+			Strict:   true,
+		},
+	}
+
+	if testSuite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run evaluation tests")
+	}
+}
